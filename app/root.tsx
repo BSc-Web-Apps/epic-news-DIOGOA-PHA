36aaf0a72ecd9f7ee499e791cce7ed108331e892
DIOGOA-PHA <Da111617@hc.ac.je>
2024-11-07 16:27:25 +0000

    The root.tsx file > changing screen background color

--- app/root.tsx
+++ app/root.tsx
@@ -14,10 +14,17 @@ export default function App() {
 
 	return (
 		<Document nonce={nonce}>
-			<div className="flex h-screen flex-col justify-between">
-				<div className="flex-1">
+			<div className="flex h-screen flex-col justify-between ">
+				<div className="flex-1 ">
 					<main className="grid h-full place-items-center">
-						<h1 className="text-mega">Welcome to Epic News!</h1>
+						<h1 className="text-mega text-yellow-300 lg:text-2xl">
+							{' '}
+							Buenos Dias !
+						</h1>
+						<p className="text-base text-yellow-300 md:text-lg lg:text-2xl">
+							Welcome to Epic News, where the latest developments in tech are
+							found.
+						</p>
 					</main>
 				</div>
 			</div>
