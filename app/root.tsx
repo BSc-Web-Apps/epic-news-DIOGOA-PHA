3558a004e96881795454af897baf850e8d072af1
DIOGOA-PHA <Da111617@hc.ac.je>
2024-11-14 14:04:30 +0000

    added 3 buttons & container code and changed the background color for the home page

--- app/root.tsx
+++ app/root.tsx
@@ -15,8 +15,8 @@ export default function App() {
 	return (
 		<Document nonce={nonce}>
 			<div className="flex h-screen flex-col justify-between ">
-				<div className="flex-1 ">
-					<main className="grid h-full place-items-center">
+				<div className="flex-1">
+					<main className="container grid h-full place-items-center bg-zinc-100">
 						<h1 className="text-mega text-yellow-300 lg:text-2xl">
 							{' '}
 							Buenos Dias !
@@ -25,6 +25,20 @@ export default function App() {
 							Welcome to Epic News, where the latest developments in tech are
 							found.
 						</p>
+						<div className="flex gap-8">
+							<button className=" bg-red-500 px-8 py-4 hover:bg-yellow-500 ">
+								{' '}
+								Learn more{' '}
+							</button>
+							<button className=" bg-red-500 px-8 py-4 hover:bg-yellow-500 ">
+								{' '}
+								Learn more{' '}
+							</button>
+							<button className=" bg-red-500 px-8 py-4 hover:bg-yellow-500 ">
+								{' '}
+								Learn more{' '}
+							</button>
+						</div>
 					</main>
 				</div>
 			</div>
