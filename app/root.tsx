21e774840689db84d04a354298cf6125e3aa5a33
DIOGOA-PHA <Da111617@hc.ac.je>
2024-11-14 15:27:53 +0000

    implemented Light and Dark mode successfully

--- app/root.tsx
+++ app/root.tsx
@@ -1,7 +1,11 @@
 import { type LinksFunction } from '@remix-run/node'
+import { useLoaderData } from '@remix-run/react'
 import Document from '~/components/shared-layout/Document'
+import ThemeSwitch from '~/components/shared-layout/ThemeSwitch'
 import { useNonce } from '~/utils/nonce-provider.ts'
 import rootLinkElements from '~/utils/providers/rootLinkElements'
+import { type loader } from './__root.server'
+import useTheme from './hooks/useTheme.tsx'
 
 export const links: LinksFunction = () => {
 	return rootLinkElements
@@ -10,13 +14,15 @@ export { headers, meta } from './__root.client.tsx'
 export { action, loader } from './__root.server.tsx'
 
 export default function App() {
+	const data = useLoaderData<typeof loader>()
 	const nonce = useNonce()
+	const theme = useTheme()
 
 	return (
-		<Document nonce={nonce}>
+		<Document nonce={nonce} theme={theme}>
 			<div className="flex h-screen flex-col justify-between ">
 				<div className="flex-1">
-					<main className="container grid h-full place-items-center bg-zinc-100">
+					<main className="container grid h-full place-items-center ">
 						<h1 className="text-mega text-yellow-300 lg:text-2xl">
 							{' '}
 							Buenos Dias !
@@ -40,6 +46,9 @@ export default function App() {
 							</button>
 						</div>
 					</main>
+				</div>
+				<div className="container flex justify-between pb-5">
+					<ThemeSwitch userPreference={data.requestInfo.userPrefs.theme} />
 				</div>
 			</div>
 		</Document>
