5d7c80cb2f37b776b6bdcff376e9eb0567037ea9
DIOGOA-PHA <Da111617@hc.ac.je>
2024-11-21 15:13:22 +0000

    finished links lesson needs more work though

--- app/components/organisms/HeaderWithSearch.tsx
+++ app/components/organisms/HeaderWithSearch.tsx
@@ -1,7 +1,8 @@
-import { useMatches } from '@remix-run/react'
+import { Link, useMatches } from '@remix-run/react'
 import NavLogo from '../molecules/NavLogo'
 import { SearchBar } from '../molecules/SearchBar'
 import LoginOrUserDropdown from './LoginOrUserDropdown'
+
 export default function HeaderWithSearch() {
 	const matches = useMatches()
 	const isOnSearchPage = matches.find(m => m.id === 'routes/users+/index')
@@ -10,7 +11,10 @@ export default function HeaderWithSearch() {
 	return (
 		<header className="dark:bg-dark-primary/10 bg-primary/10 py-6">
 			<nav className="container flex flex-wrap items-center justify-between gap-4 sm:flex-nowrap md:gap-8">
-				<NavLogo />
+				<Link to="/">
+					<NavLogo />
+				</Link>
+
 				<div className="ml-auto hidden max-w-sm flex-1 sm:block">
 					{searchBar}
 				</div>
