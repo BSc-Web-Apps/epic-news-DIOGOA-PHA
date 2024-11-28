64914dbc9b7a9f087653823877d9278a9951883e
DIOGOA-PHA <Da111617@hc.ac.je>
2024-11-28 16:41:12 +0000

    bug fixed

--- app/routes/news.$category.tsx
+++ app/routes/news.$category.tsx
@@ -1,3 +1,4 @@
+import { invariant } from '@epic-web/invariant'
 import { type LoaderFunctionArgs, json } from '@remix-run/node'
 import { useLoaderData } from '@remix-run/react'
 import { toTitleCase } from '~/utils/stringUtils.ts'
@@ -5,6 +6,7 @@ import { toTitleCase } from '~/utils/stringUtils.ts'
 export async function loader({ params }: LoaderFunctionArgs) {
 	const { category } = params
 
+	invariant(typeof category === 'string', 'Category not found')
 	const categoryTitle = toTitleCase(category)
 	return json({ categoryTitle })
 }
@@ -14,13 +16,6 @@ export default function NewsCategoryPage() {
 	return (
 		<div className="container py-16">
 			<h2 className="text-h2">{categoryTitle}</h2>
-		</div>
-	)
-}
-export default function NewsCategoryPage() {
-	return (
-		<div className="container py-16">
-			<h2 className="text-h2">Generic news category page</h2>
 		</div>
 	)
 }
