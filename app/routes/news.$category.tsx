790631ce35f2d570b2a864e4bc13e2d1a05887d2
DIOGOA-PHA <Da111617@hc.ac.je>
2024-11-28 16:28:28 +0000

    working on server/client communication got a bug

--- app/routes/news.$category.tsx
+++ app/routes/news.$category.tsx
@@ -1,3 +1,22 @@
+import { type LoaderFunctionArgs, json } from '@remix-run/node'
+import { useLoaderData } from '@remix-run/react'
+import { toTitleCase } from '~/utils/stringUtils.ts'
+
+export async function loader({ params }: LoaderFunctionArgs) {
+	const { category } = params
+
+	const categoryTitle = toTitleCase(category)
+	return json({ categoryTitle })
+}
+export default function NewsCategoryPage() {
+	const { categoryTitle } = useLoaderData<typeof loader>()
+
+	return (
+		<div className="container py-16">
+			<h2 className="text-h2">{categoryTitle}</h2>
+		</div>
+	)
+}
 export default function NewsCategoryPage() {
 	return (
 		<div className="container py-16">
