cb1e1df665ddec7b21c20be0b9acffa97d04e6df
DIOGOA-PHA <Da111617@hc.ac.je>
2024-12-12 13:16:20 +0000

    starting display article data

--- app/routes/news.$category.tsx
+++ app/routes/news.$category.tsx
@@ -2,13 +2,22 @@ import { invariant } from '@epic-web/invariant'
 import { type LoaderFunctionArgs, json } from '@remix-run/node'
 import { useLoaderData } from '@remix-run/react'
 import { toTitleCase } from '~/utils/stringUtils.ts'
+import { prisma } from '~/utils/db.server.ts'
 
 export async function loader({ params }: LoaderFunctionArgs) {
 	const { category } = params
 
 	invariant(typeof category === 'string', 'Category not found')
 	const categoryTitle = toTitleCase(category)
-	return json({ categoryTitle })
+	const allArticles = await prisma.article.findMany({
+		select: {
+		  id: true,
+		  title: true,
+		  category: { select: { name: true } },
+		  images: { select: { id: true } },
+		},
+	  })
+	return json({ categoryTitle, allArticles })
 }
 export default function NewsCategoryPage() {
 	const { categoryTitle } = useLoaderData<typeof loader>()
